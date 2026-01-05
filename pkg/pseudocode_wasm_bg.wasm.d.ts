029e1dc056261ce4cc9d68570842be101da42813
oJackyToasto <jt31415926535@outlook.com>
2026-01-05 13:50:01 +0800

    Deploy to GitHub Pages

---
+++ pkg/pseudocode_wasm_bg.wasm.d.ts
@@ -0,0 +1,27 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_pseudocodeengine_free: (a: number, b: number) => void;
+export const init: () => void;
+export const pseudocodeengine_add_input: (a: number, b: number, c: number) => void;
+export const pseudocodeengine_check_syntax: (a: number, b: number, c: number) => any;
+export const pseudocodeengine_clear_inputs: (a: number) => void;
+export const pseudocodeengine_execute: (a: number, b: number, c: number) => any;
+export const pseudocodeengine_execute_next_statement: (a: number) => any;
+export const pseudocodeengine_get_completions: (a: number, b: number, c: number, d: number, e: number) => any;
+export const pseudocodeengine_get_hover: (a: number, b: number, c: number, d: number, e: number) => any;
+export const pseudocodeengine_get_input_statements: (a: number, b: number, c: number) => any;
+export const pseudocodeengine_get_next_statement_info: (a: number) => any;
+export const pseudocodeengine_get_virtual_file: (a: number, b: number, c: number) => [number, number];
+export const pseudocodeengine_has_more_statements: (a: number) => number;
+export const pseudocodeengine_new: () => number;
+export const pseudocodeengine_parse_for_execution: (a: number, b: number, c: number) => any;
+export const pseudocodeengine_set_virtual_file: (a: number, b: number, c: number, d: number, e: number) => void;
+export const pseudocodeengine_validate_input_variable: (a: number, b: number, c: number) => [number, number];
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
